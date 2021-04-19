edc176e7740ff7e20f05d9601f3b0e96f9a2a81c
Henrique Assis <henrique.assis145@gmail.com>
2021-04-19 14:24:29 -0300

    logout

--- MarketPlace/frontend/src/componentes/partials/Header/styled.js
+++ MarketPlace/frontend/src/componentes/partials/Header/styled.js
@@ -58,10 +58,15 @@ export const HeaderArea = styled.div `
             margin-left:20px;
             margin-right:20px;
 
-            a{
+            a, 
+            button{
+                border:0;
+                background:none;
                 color:#000;
                 font-size:14px;
                 text-decoration:none;
+                cursor:pointer;
+                outline:0;
 
                 &:hover{
                     color:#2980B9;
--- MarketPlace/frontend/src/helpers/AuthHandler.js
+++ MarketPlace/frontend/src/helpers/AuthHandler.js
@@ -1,14 +1,21 @@
 import Cookies from 'js-cookie';
 
+//Verifica se esta logado
 export const isLogged = () =>{
     let token = Cookies.get('token');
     return(token) ? true : false;
 }
 
+//Faz o Login
 export const doLogin = (token, rememberPassword = false) =>{
     if(rememberPassword){
         Cookies.set('token', token, {expires:365});
     }else{
         Cookies.set('token', token)
     }
+}
+
+//Faz o Logout
+export const doLogout = () =>{
+    Cookies.remove('token');
 }
